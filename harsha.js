3992650118051d94ab793176e18b9f21e0651761
HarshavardhanaP96 <68232905+HarshavardhanaP96@users.noreply.github.com>
2021-11-29 12:25:28 +0530

    Add files via upload

---
+++ harsha.js
@@ -0,0 +1,47 @@
+// JavaScript Document
+var para=document.getElementById("para");
+function red()
+{
+	para.style.color="red";
+	
+}
+function green()
+{
+	para.style.color="green";
+	
+}
+function blue()
+{
+	para.style.color="blue";
+	
+}
+function size10()
+{
+	para.style.fontSize="10px";
+	
+}
+function size20()
+{
+	para.style.fontSize="20px";
+	
+}
+function size30()
+{
+	para.style.fontSize="30px";
+	
+}
+function lato()
+{
+	para.style.fontFamily="lato";
+	
+}
+function raleway()
+{
+	para.style.fontFamily="raleway";
+	
+}
+function arial()
+{
+	para.style.fontFamily="arial";
+	
+}
